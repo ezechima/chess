096cde532eb4757f7958c4d870edc27dab7c0996
Chima Ezenwanne <ezechima@gmail.com>
2017-06-25 20:07:39 +0100

    implement ChessSessionControlller methods

--- lib/controllers/chess_session_controller.rb
+++ lib/controllers/chess_session_controller.rb
@@ -1,16 +1,34 @@
 module ChimaChess
 	class ChessSessionController
-		def create_new_session
-			
+		attr_accessor :session_manager, :state_controller
+
+		def initialize
+			create_session_manager
+
+		end
+		def new_session
+			session = session_manager.new_session
+			create_state_controller(session)
+
 		end
 		def load_session
+			session = session_manager.load_session
+			create_state_controller(session)
 		end
 		def stop_session
 		end
 		def save_session
+			session_manager.save_session
 		end
-		
+
+		def create_session_manager
+			session_manager = ChimaChess::SessionManager.new()
+
+		end
+		def create_state_controller(session)
+			state_controller = ChimaChess::ChessStateController.new(session)
+
+		end
+
 	end
 end
-
-
