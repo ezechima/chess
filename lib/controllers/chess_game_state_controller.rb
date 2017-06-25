102e3b53a8458fd9c9a31d28e81dff7fe8bbe38f
Chima Ezenwanne <ezechima@gmail.com>
2017-06-25 18:58:55 +0100

    Map State controller methods to state monitor

--- lib/controllers/chess_game_state_controller.rb
+++ lib/controllers/chess_game_state_controller.rb
@@ -1,12 +1,33 @@
 module ChimaChess
 	class ChessGameStateController
-		def revert_game_state
+		attr_accessor :state_monitor
+		def initialize(state_monitor)
+			@state_monitor = state_monitor
 		end
-		def undo_game_state
-			
+
+		def process_request(request)
+			request_id = request.request_id
+			send("#{request_id}")
+
 		end
-		def redo_game_state
-			
+		def reset
+			state_monitor.reset_state
+		end
+		def undo
+			state_monitor.previous_state
+
+		end
+		def redo
+			state_monitor.next_state
+
+		end
+		def commit(state)
+			state_monitor.add_state(state)
+
+		end
+		def move
+			new_state = move_controller.process_request(request)
+			commit(new_state)
 		end
 	end
 end
