3dc55f3aded008bfcc6cb87afb883d4c130e7384
Chima Ezenwanne <ezechima@gmail.com>
2017-06-25 21:05:02 +0100

    Creates new board and new StateMonitor

--- lib/models/session_manager.rb
+++ lib/models/session_manager.rb
@@ -1,19 +1,31 @@
-class SessionManager
-	attr_accessor :active_session
-	def initialize (session_writer:, session_reader:,session_creator:)
+module ChimaChess
+  require './lib/controllers/board_creator.rb'
+
+	class SessionManager
+		attr_accessor :active_session
+
+		def new_session
+			active_session = create_new_session
+		end
+
+		def load_session
+			#active_session = session_creator.load
+		end
+
+		def save_session
+
+			#session_writer.write(active_session)
+		end
+		def create_new_session
+			board = board_creator.create_new_board
+			session = create_state_monitor(board)
+			session
+		end
+		def create_new_board
+			ChimaChess::BoardCreator.create()
+		end
+		def create_state_monitor(board)
+			ChimaChess::StateMonitor.new(board)
+		end
 	end
-
-	def new_session
-		active_session = session_creator.create
-	end
-
-	def load_session
-		active_session = session_creator.load
-	end
-
-	def save_session
-		session_writer.write(active_session)
-	end
-
-
 end
