42341d1d842c72a0c6a2842e5a2462d268ca85cd
Chima Ezenwanne <ezechima@gmail.com>
2017-06-26 15:09:45 +0100

    Add formating

--- lib/controllers/chess_application.rb
+++ lib/controllers/chess_application.rb
@@ -1,16 +1,19 @@
 module ChimaChess
   class ChessApplication
     attr_reader :session_controller
+
     def initialize
         @session_controller = create_session_controller
     end
+
     def create_session_controller(self)
      ChimaChess::ChessSessionController.new(self)
     end
+
     def current_state
       session_controller.current_state
-
     end
+    
     def exit
       exit(true)
     end
