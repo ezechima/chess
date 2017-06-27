de285b22ebd8e9f6dcf8d10625dafe7550f64835
Chima Ezenwanne <ezechima@gmail.com>
2017-06-27 02:23:46 +0100

    implement move control methods

--- lib/controllers/move_controller.rb
+++ lib/controllers/move_controller.rb
@@ -5,14 +5,47 @@ module ChimaChess
       clear(destination,board)
       board.tile(destination).piece = piece
       clear(src,board)
+      piece.has_moved = true
     end
     def self.clear(tile_loc,board)
       board.tile(tile_loc).clear_piece
     end
   end
 
-  class PawnMoveController
-    
+  class PawnMoveController < RegularMoveController
+
+    def self.process(message,state)
+      src_loc = find_piece_loc(message,state)
+
+      if message.destination == state.enpassant_tile
+        kill_enpassant(state)
+        state.set_enpassant_tile(nil)
+      elsif can_set_enpassant?(src,message.destination,state)
+        set_enpassant(src,message.destination,state)
+      else
+        state.set_enpassant_tile(nil)
+      end
+
+      move_piece(src_loc,message.destination,state)
+      king_check(state)
+    end
+
+    def self.kill_enpassant(state)
+      state.tile(state.enpassant_victim).clear_piece
+    end
+
+    def self.can_set_enpassant?(src,destination,state)
+      (destination[1] == "4") || (destination[1] == "5") && !state.piece(src).has_moved
+    end
+
+    def self.set_enpassant(src,destination,state)
+      enpassant_rank = (destination[1].to_i + src[1].to_i)/2
+      enpassant_file =  destination[0]
+      enpassant_tile = "#{enpassant_file}#{enpassant_rank}"
+
+      state.set_enpassant_tile(enpassant_tile)
+      state.set_enpassant_victim(destination)
+    end
   end
 
   class RegularMoveController
@@ -27,9 +60,27 @@ module ChimaChess
       ChimaChess::PawnMoveController.process(message,state)
     end
     def self.process_regular_move(message,state)
-      find_piece
-      move_piece
-      king_check
+      src_loc = find_piece_loc(message,state)
+      move_piece(src_loc,message.destination,state)
+      king_check(state)
+      state.set_enpassant_tile(nil)
+    end
+    def self.find_piece_loc(message,state)
+      piece_hash = ChimaChess::FindPiece.find(
+                      board: state,
+                      color: state.turn_to_play,
+                      type: message.piece_type,
+                      destination_str: message.destination,
+                      file: message.file,
+                      rank: message.rank
+                    )
+      piece_hash.values[0].attacker_location
+    end
+    def self.king_check(state)
+      ChimaChess::KingChecker.check(state)
+    end
+    def self.move_piece(src,destination,board)
+      ChimaChess::PieceMover.move_piece(src,destination,board)
     end
   end
 
@@ -38,18 +89,18 @@ module ChimaChess
       castle_positions = check_castle(message,state)
       move_castle_pieces(castle_positions,state)
     end
-    def check_castle(message,state)
+    def self.check_castle(message,state)
       ChimaChess::CastleCheck.check(
         side: message.castle_side,
         color: state.turn_to_play,
         board: state
         )
     end
-    def move_castle_pieces(castle_positions,state)
+    def self.move_castle_pieces(castle_positions,state)
       move_piece(castle_positions[:king_position],castle_positions[:king_destination],state)
       move_piece(castle_positions[:rook_position],castle_positions[:rook_destination],state)
     end
-    def move_piece(src,destination,board)
+    def self.move_piece(src,destination,board)
       ChimaChess::PieceMover.move_piece(src,destination,board)
     end
   end
